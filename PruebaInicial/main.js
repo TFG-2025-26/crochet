570ca5420cac9e13ce0e1434245f4b18afc1e016
Erisolo <marsolor@ucm.es>
2026-01-18 02:49:53 +0100

    esto de no hacer comit no vale eh

--- PruebaInicial/main.js
+++ PruebaInicial/main.js
@@ -1,25 +1,38 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();    //creamos la escena
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );    //creamos la cámara
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/2 / window.innerHeight, 0.1, 1000 );    //creamos la cámara
                                         //( field of view,  aspect ratio, near and far clipping)
 
 const renderer = new THREE.WebGLRenderer({ alpha: true }); //creamos el renderer    //el alpha true para q tenga en cuenta el alfa
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize( window.innerWidth/2, window.innerHeight );
 
 //me molestaba q el fondo fuese negro asiq vamos a cambiarlo
 //scene.background = new THREE.Color( 0x000000, 0); //esto no está funcionando
 renderer.setClearColor(0x000000, 0);  //haleluja  
 
-//esto no entiendo exactamente q hace pero un besazo supongo
-document.body.appendChild( renderer.domElement );
+
+
+//AHHH ESTO SELECCIONA Q PARTE DEL DOMUENTO VAMOS A PONER EL CANVAS
+document.querySelector("#panes>.right").appendChild( renderer.domElement );
+
+//q majos son, q tienen los controles built in
+const controls = new OrbitControls( camera, renderer.domElement );
 
 //ahora a crear nuestro cubo
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );  //aqui se asigna el color
+const material = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );  //aqui se asigna el color
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
+
+//ponemos una luz tontorrona para poder ver mejor
+   const skyColor = 0xffffff;
+const groundColor = 0x222222;  
+const intensity = 1;
+const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
+scene.add(light);
 
 camera.position.z = 5;
 
@@ -27,8 +40,9 @@ camera.position.z = 5;
 //y aqui renderizamos nuestro cubo
 function animate() {
   //VAMOS A ANIMAR EL CUBO???
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  //cube.rotation.x += 0.01;
+  //cube.rotation.y += 0.01;
+  controls.update();
 
   renderer.render( scene, camera );
 }
